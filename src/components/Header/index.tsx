bf80242785e17202a7ac7a5dfb1b3b2113e59e3e
PresentKim <debe3721@gmail.com>
2025-02-15 17:10:06 +0900

    refactor(app): Restructure application layout and remove unused assets

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="flex justify-between w-full bg-neutral-800 text-neutral-200 shadow-xs shadow-neutral-900 min-h-8 px-4 py-2 align-middle">
+      <p>Header Left</p>
+      <p>Header Right</p>
+    </header>
+  )
+}
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,7 @@
+export default function Main() {
+  return (
+    <main className="flex flex-col items-center justify-center flex-1 w-full mx-auto">
+      <h1 className="text-4xl font-bold my-2 text-center">Main</h1>
+    </main>
+  )
+}
