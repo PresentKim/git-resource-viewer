61f9f2f8fe4559689ed0507b38294034e5b8585b
PresentKim <debe3721@gmail.com>
2025-02-15 17:10:24 +0900

    feat(footer): Update footer with links to GitHub profile and repository

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,20 @@
 export default function Footer() {
   return (
-    <footer className="flex items-center justify-center w-full my-4">
-      <p>Footer</p>
+    <footer className="flex items-center justify-center w-full my-4 gap-0.5 text-xs text-neutral-500">
+      <a
+        href="https://github.com/PresentKim"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="font-bold">
+        PresentKim
+      </a>
+      <p>/</p>
+      <a
+        href="https://github.com/PresentKim/repo-image-viewer"
+        target="_blank"
+        rel="noopener noreferrer">
+        repo-image-viewer
+      </a>
     </footer>
   )
 }
