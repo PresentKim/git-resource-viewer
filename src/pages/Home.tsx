0d470d16779241d9d8a059a6d74ed7cba1b166c1
PresentKim <debe3721@gmail.com>
2025-02-16 20:46:05 +0900

    feat(home): Enhance Home page with search functionality and example repository button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,12 @@
-import {ImagesIcon as HeaderIcon} from 'lucide-react'
+import {ImagesIcon as HeaderIcon, PickaxeIcon, SearchIcon} from 'lucide-react'
 import {Header, HeaderSide, HeaderTitle} from '@/components/header'
+import {Button} from '@/components/ui/button'
+import {useTargetRepository} from '@/hooks/useTargetRepository'
+import {useSearchDialogStore} from '@/stores/searchDialogStore'
 
 export default function Home() {
+  const {open: openSearchDialog} = useSearchDialogStore()
+  const [, setTargetRepository] = useTargetRepository()
   return (
     <>
       <Header>
@@ -12,7 +17,31 @@ export default function Home() {
         <HeaderSide> </HeaderSide>
       </Header>
       <main className="flex flex-col flex-1 justify-start items-center min-h-8 w-full px-4 py-8 space-y-6">
-        <h1 className="my-2 text-center text-4xl font-bold">Home</h1>
+        <h1 className="text-center text-3xl font-bold">
+          Github Repository Image Viewer
+        </h1>
+        <p className="text-center text-lg text-neutral-400 max-w-2xl">
+          Browse and view all images in GitHub repository at once. Simply enter
+          a repository URL to get started.
+        </p>
+        <Button onClick={openSearchDialog} className="ring-neutral-400">
+          <SearchIcon className="size-5 mr-2" />
+          Search Repository
+        </Button>
+        <p className="text-center text-neutral-400">or</p>
+        <Button
+          onClick={() =>
+            setTargetRepository({
+              // https://github.com/Mojang/bedrock-samples
+              owner: 'Mojang',
+              repo: 'bedrock-samples',
+              ref: 'main',
+            })
+          }
+          className="ring-neutral-400">
+          <PickaxeIcon strokeWidth={3} />
+          Open example repository
+        </Button>
       </main>
     </>
   )
