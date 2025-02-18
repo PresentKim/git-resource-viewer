5fb37f8a67afa1d6059ee8079eec28911ef79700
PresentKim <debe3721@gmail.com>
2025-02-18 12:38:19 +0900

    feat: Replace Loading component with inline fallback in Suspense for improved loading experience

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import {Suspense, lazy} from 'react'
 import {HashRouter as Router, Routes, Route} from 'react-router-dom'
+import {LoaderIcon} from 'lucide-react'
 import BaseLayout from '@/layout/BaseLayout'
 
 const LazyHome = lazy(() => import('@/pages/Home'))
 const LazySettings = lazy(() => import('@/pages/Settings'))
 const LazyRepoView = lazy(() => import('@/pages/RepoView'))
 const LazyNotFound = lazy(() => import('@/pages/NotFound'))
-const LazyLoading = lazy(() => import('@/pages/Loading'))
 
 function App() {
   return (
-    <Suspense fallback={<LazyLoading />}>
+    <Suspense
+      fallback={
+        <main className="flex items-center justify-center">
+          <LoaderIcon className="size-16 animate-spin" />
+        </main>
+      }>
       <Router>
         <Routes>
           <Route element={<BaseLayout />}>
--- src/pages/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import {LoaderIcon} from 'lucide-react'
-
-export default function Loading() {
-  return (
-    <main className="flex items-center justify-center h-full">
-      <LoaderIcon className="size-16 animate-spin" />
-    </main>
-  )
-}
