594e42c24b9c13c718eb496df08aa8a93f66b213
PresentKim <debe3721@gmail.com>
2025-02-19 01:02:26 +0900

    feat(utils): Add throttle function for rate limiting
    - Implemented a throttle function to limit the execution rate of a given function.
    - Helps improve performance by preventing excessive calls within a specified delay.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,19 @@ import {twMerge} from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function throttle<T extends unknown[]>(
+  fn: (...args: T) => void,
+  delay: number,
+) {
+  let timeout: NodeJS.Timeout | null = null
+
+  return (...args: T) => {
+    if (!timeout) {
+      timeout = setTimeout(() => {
+        fn(...args)
+        timeout = null
+      }, delay)
+    }
+  }
+}
