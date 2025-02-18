33a32257bad85c5215afdc24c220866665f6cc2b
PresentKim <debe3721@gmail.com>
2025-02-18 15:43:42 +0900

    feat(repo-view): Implement loading states and refactor image fetching
    - Added loading indicators for default branch and image files.
    - Refactored image fetching logic to utilize the usePromise hook.
    - Improved user experience by displaying appropriate messages when data is loading or not found.

--- src/pages/RepoView.tsx
+++ src/pages/RepoView.tsx
@@ -11,65 +11,71 @@ import {
   useGithubImageFileTree,
   type GithubImageFile,
 } from '@/hooks/useGithubApi'
+import {usePromise} from '@/hooks/usePromise'
+
+function LoadingBranch() {
+  return <div>Loading default branch...</div>
+}
+
+function LoadingImageFiles() {
+  return <div>Loading image files...</div>
+}
+
+function NoImageFiles() {
+  return <div>No image files found</div>
+}
 
 export default function RepoView() {
   const [{owner, repo, ref}, setTargetRepository] = useTargetRepository()
-  const fetchGithubDefaultBranch = useGithubDefaultBranch()
-  const fetchGithubImageFileTree = useGithubImageFileTree()
-  const [githubImageFiles, setGithubImageFiles] = useState<
-    GithubImageFile[] | null
-  >(null)
+  const [isLoadRef, getDefaultBranch] = usePromise(useGithubDefaultBranch())
+  const [isLoadImagePaths, getImagePaths] = usePromise(useGithubImageFileTree())
+  const [imageFiles, setImageFiles] = useState<GithubImageFile[] | null>(null)
   const [imageSize] = useState(64)
 
   useEffect(() => {
     if (owner && repo && !ref) {
-      fetchGithubDefaultBranch(owner, repo)
+      getDefaultBranch(owner, repo)
         .then(defaultBranch => setTargetRepository(owner, repo, defaultBranch))
         .catch(console.error)
     } else {
-      fetchGithubImageFileTree(owner, repo, ref)
-        .then(imageFileTree => setGithubImageFiles(imageFileTree))
+      getImagePaths(owner, repo, ref)
+        .then(imageFileTree => setImageFiles(imageFileTree))
         .catch(console.error)
     }
-  }, [
-    owner,
-    repo,
-    ref,
-    fetchGithubDefaultBranch,
-    setTargetRepository,
-    fetchGithubImageFileTree,
-  ])
+  }, [owner, repo, ref, getDefaultBranch, setTargetRepository, getImagePaths])
+
+  if (isLoadRef) {
+    return <LoadingBranch />
+  } else if (isLoadImagePaths) {
+    return <LoadingImageFiles />
+  } else if (!imageFiles || !imageFiles.length) {
+    return <NoImageFiles />
+  }
 
   return (
-    <>
-      <div className="flex flex-wrap gap-4">
-        {githubImageFiles && githubImageFiles.length ? (
-          githubImageFiles.map((path, i) => (
-          <TooltipProvider key={i}>
-            <Tooltip>
-              <TooltipTrigger
-                className="aspect-square relative select-none hover:ring-2 ring-white hover:rounded-xs transition-all"
-                style={{width: imageSize, height: imageSize}}>
-                <img
-                  src={`https://raw.githubusercontent.com/${owner}/${repo}/${ref}/${path}`}
-                  alt={path}
-                  loading="lazy"
-                  className="w-full h-full object-contain"
-                />
-              </TooltipTrigger>
-              <TooltipContent
-                side="bottom"
-                sticky="partial"
-                className="min-w-fit p-2 rounded-md bg-neutral-900 text-neutral-100 ring-1 ring-neutral-700">
-                <p className="px-2 py-1">{path}</p>
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-          ))
-        ) : (
-          <div>No image files found</div>
-        )}
-      </div>
-    </>
+    <div className="flex flex-wrap gap-4">
+      {imageFiles.map((path, i) => (
+        <TooltipProvider key={i}>
+          <Tooltip>
+            <TooltipTrigger
+              className="aspect-square relative select-none hover:ring-2 ring-white hover:rounded-xs transition-all"
+              style={{width: imageSize, height: imageSize}}>
+              <img
+                src={`https://raw.githubusercontent.com/${owner}/${repo}/${ref}/${path}`}
+                alt={path}
+                loading="lazy"
+                className="w-full h-full object-contain"
+              />
+            </TooltipTrigger>
+            <TooltipContent
+              side="bottom"
+              sticky="partial"
+              className="min-w-fit p-2 rounded-md bg-neutral-900 text-neutral-100 ring-1 ring-neutral-700">
+              <p className="px-2 py-1">{path}</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      ))}
+    </div>
   )
 }
