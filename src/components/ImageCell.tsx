26db4940f42a7966dc9013498825e21b4a7f9d44
PresentKim <debe3721@gmail.com>
2025-03-05 11:45:24 +0900

    feat(image): Add ImageCell component for displaying images
    - Introduced ImageCell component to fetch and display images from a repository.
    - Added loading state with a spinner while images are being fetched.

---
+++ src/components/ImageCell.tsx
@@ -0,0 +1,74 @@
+import {useCallback, useEffect, useState, memo} from 'react'
+import {LoaderCircleIcon} from 'lucide-react'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip'
+
+interface ImageCellProps {
+  owner: string
+  repo: string
+  ref: string
+  path: string
+}
+
+function Image({owner, repo, ref, path}: ImageCellProps) {
+  const [loading, setLoading] = useState(false)
+
+  const handleLoad = useCallback(() => {
+    setLoading(false)
+  }, [])
+
+  useEffect(() => {
+    setLoading(true)
+  }, [path])
+
+  return (
+    <>
+      <div
+        className="size-full opacity-10 ring-muted-foreground ring-1 rounded-md"
+        style={{display: loading ? 'block' : 'none'}}>
+        <LoaderCircleIcon className="size-full object-contain text-muted animate-spin duration-[3s]" />
+      </div>
+      <img
+        src={`https://raw.githubusercontent.com/${owner}/${repo}/${ref}/${path}`}
+        alt={path}
+        className="size-full object-contain"
+        onLoad={handleLoad}
+        style={{display: loading ? 'none' : 'block'}}
+      />
+    </>
+  )
+}
+
+const ImageCell = memo(function ImageCell({
+  owner,
+  repo,
+  ref,
+  path,
+}: ImageCellProps) {
+  const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0
+
+  return isTouchDevice ? (
+    <div className="aspect-square size-full ring-foreground transition-all active:ring-2 active:rounded-xs">
+      <Image owner={owner} repo={repo} ref={ref} path={path} />
+    </div>
+  ) : (
+    <TooltipProvider key={path}>
+      <Tooltip>
+        <TooltipTrigger className="aspect-square size-full ring-foreground transition-all hover:ring-2 hover:rounded-xs">
+          <Image owner={owner} repo={repo} ref={ref} path={path} />
+        </TooltipTrigger>
+        <TooltipContent
+          side="bottom"
+          sticky="partial"
+          className="min-w-fit p-2 rounded-md bg-card text-card-foreground ring-1 ring-muted-foreground">
+          <p className="px-2 py-1">{path}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  )
+})
+export {ImageCell}
--- src/pages/RepoView.tsx
+++ src/pages/RepoView.tsx
@@ -1,16 +1,11 @@
-import {useCallback, useEffect, useMemo, useState, memo} from 'react'
+import {useCallback, useEffect, useMemo, useState} from 'react'
 import {GitBranchPlusIcon, ImageIcon, LoaderIcon} from 'lucide-react'
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from '@/components/ui/tooltip'
 import {RandomMessageLoader} from '@/components/RandomMessageLoader'
 import {
   VirtualizedFlexGrid,
   type RenderData,
 } from '@/components/VitualizedFlexGrid'
+import {ImageCell} from '@/components/ImageCell'
 import {useGithubDefaultBranch} from '@/api/github/hooks/useGithubDefaultBranch'
 import {useGithubImageFileTree} from '@/api/github/hooks/useGithubImageFileTree'
 import type {GithubImageFileTree} from '@/api/github/types'
@@ -22,50 +17,6 @@ import {
   generateImageFetchMessage,
   generateNoImagesMessage,
 } from '@/utils/randomMessages'
-
-interface ImageCellProps {
-  owner: string
-  repo: string
-  ref: string
-  path: string
-}
-
-const ImageCell = memo(function ImageCell({
-  owner,
-  repo,
-  ref,
-  path,
-}: ImageCellProps) {
-  const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0
-
-  return isTouchDevice ? (
-    <div className="aspect-square size-full ring-foreground transition-all active:ring-2 active:rounded-xs">
-      <img
-        src={`https://raw.githubusercontent.com/${owner}/${repo}/${ref}/${path}`}
-        alt={path}
-        className="size-full object-contain"
-      />
-    </div>
-  ) : (
-    <TooltipProvider key={path}>
-      <Tooltip>
-        <TooltipTrigger className="aspect-square size-full ring-foreground transition-all hover:ring-2 hover:rounded-xs">
-          <img
-            src={`https://raw.githubusercontent.com/${owner}/${repo}/${ref}/${path}`}
-            alt={path}
-            className="size-full object-contain"
-          />
-        </TooltipTrigger>
-        <TooltipContent
-          side="bottom"
-          sticky="partial"
-          className="min-w-fit p-2 rounded-md bg-card text-card-foreground ring-1 ring-muted-foreground">
-          <p className="px-2 py-1">{path}</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
-  )
-})
 
 export default function RepoView() {
   const [{owner, repo, ref}, setTargetRepository] = useTargetRepository()
